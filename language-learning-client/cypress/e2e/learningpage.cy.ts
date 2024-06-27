a513edee86c7af51a11d0a2d5d9c84122c0e2986
Phat Nguyen <phattrong.a@gmail.com>
2024-06-27 12:45:15 +0300

    Modify learningpage.cy.ts

--- language-learning-client/cypress/e2e/learningpage.cy.ts
+++ language-learning-client/cypress/e2e/learningpage.cy.ts
@@ -1,6 +1,6 @@
 describe('Deck details test', function() {
     beforeEach(function() {
-        cy.visit('http://localhost:5173/')
+        cy.visit('https://padwhen-learningapp.fly.dev')
         cy.contains('Log In').click()
         cy.get('#username').type('1111')
         cy.get('[data-testid="pin-input"]') 
