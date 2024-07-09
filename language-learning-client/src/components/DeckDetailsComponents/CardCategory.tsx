699f283b2d3ec58ad947ec082ef3e432aa704253
Phat Nguyen <phattrong.a@gmail.com>
2024-07-09 11:54:36 +0300

    deck details page mobile responsive & modify pipeline.yml

--- language-learning-client/src/components/DeckDetailsComponents/CardCategory.tsx
+++ language-learning-client/src/components/DeckDetailsComponents/CardCategory.tsx
@@ -8,27 +8,27 @@ type CardCategoryProps = {
 
 export const CardCategory = ({ categoryName, cards, id }: CardCategoryProps) => {
     return (
-        <div className="flex flex-col gap-1">
-            <h2 className="text-2xl font-bold text-pink-500 pt-3">{categoryName} ({cards.length})</h2>
+        <div className="flex flex-col gap-1 w-full">
+            <h2 className="text-xl sm:text-2xl font-bold text-pink-500 pt-3">{categoryName} ({cards.length})</h2>
             {cards.length > 0 ? (
                 <>
                     {categoryName === 'Still learning' && (
-                        <h3 className="text-gray-500 text-lg">You've begun learning these terms. Keep up the good work!</h3>
+                        <h3 className="text-gray-500 text-base sm:text-lg">You've begun learning these terms. Keep up the good work!</h3>
                     )}
                     {categoryName === 'Not studied' && (
-                        <h3 className="text-gray-500 text-lg">You haven't studied these terms yet.</h3>
+                        <h3 className="text-gray-500 text-base sm:text-lg">You haven't studied these terms yet.</h3>
                     )}
                     {categoryName === 'Completed' && (
-                        <h3 className="text-gray-500 text-lg">Congratulations!</h3>
+                        <h3 className="text-gray-500 text-base sm:text-lg">Congratulations!</h3>
                     )}
-                    <div className="flex flex-col gap-3 mt-2">
+                    <div className="flex flex-col gap-2 sm:gap-3 mt-2">
                         {cards.map((card: any) => (
                             <Word cardId={card._id} key={card._id} deckId={id} engCard={card.engCard} userLangCard={card.userLangCard} />
                         ))}
                     </div>
                 </>
             ) : (
-                <h3 className="text-gray-500 text-md">There are no cards in "{categoryName}".</h3>
+                <h3 className="text-gray-500 sm:text-md text-sm">There are no cards in "{categoryName}".</h3>
             )}
         </div>
     );
--- language-learning-client/src/components/FlashCardComponents/BackCard.tsx
+++ language-learning-client/src/components/FlashCardComponents/BackCard.tsx
@@ -9,16 +9,16 @@ interface Props {
 
 export const BackCard = ({ word }: Props) => {
     return (
-        <div className="h-[350px] shadow-md">
-            <header className="mt-2 ml-2 flex justify-between items-center">
-                <div className="inline-flex flex-row gap-2 items-center justify-center ml-5 mt-2">
+        <div className="min-h-[250px] md:h-[350px] shadow-md">
+            <header className="mt-2 ml-2 flex justify-start items-center">
+                <div className="inline-flex flex-row gap-2 items-center justify-center sm:ml-5 ml-2 mt-2">
                     <ToolTip trigger={<Pencil />} content="Edit the flashcard" />
                     <ToolTip trigger={<Volume1 />} content="Speak the word!" />
                     <ToolTip trigger={<Star />} content="Mark as favorite" />
                 </div>
             </header>
-            <div className="flex items-center justify-center h-[250px]">
-                <h1 className="text-7xl">{word}</h1>
+            <div className="flex items-center justify-center h-[150px] sm:h-[250px]">
+                <h1 className="text-4xl sm:text-5xl md:text-7xl text-center px-4">{word}</h1>
             </div>
         </div>
     )
