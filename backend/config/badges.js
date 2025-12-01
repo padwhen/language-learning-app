ac9259e6c4f6c0ed8881db6eeae9c809b607f93c
Phat Nguyen <123895854+padwhen@users.noreply.github.com>
2025-12-01 22:39:52 +0200

    chore: remove backend files and configurations in preparation for project restructuring

--- backend/config/badges.js
+++
@@ -1,8 +0,0 @@
-const badgeMilestones = [
-    { xp: 10000, name: 'Bronze XP', tier: 'Bronze' },
-    { xp: 50000, name: 'Silver XP', tier: 'Silver' },
-    { xp: 100000, name: 'Gold XP', tier: 'Gold' },
-    { xp: 500000, name: 'Platinum XP', tier: 'Platinum' }
-];
-
-module.exports = badgeMilestones
